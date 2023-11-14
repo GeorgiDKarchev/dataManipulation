78412b9c373d30ac08ead63ee7306f7e7021f8b9
Militsa Neykova <militsaneykova@Militsas-MBP.lan>
2023-11-13 22:10:39 -0500

    Math problems task JavaSkript

--- dataManipulation/index.js
+++ dataManipulation/index.js
@@ -8,29 +8,38 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log(isSum50)
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(n1 % 2);
+console.log(n2 % 2);
+console.log(n3 % 2);
+console.log(n4 % 2);
+console.log(isTwoOdd);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log(isOver25);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(isUnique);
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+console.log(isValid);
 
 // Finally, log the results.
 console.log(isValid);
@@ -43,3 +52,29 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  
+  //Check if all # divisible by 5
+  const isDivisibleBy5=(n1%5) + (n2%5) + (n3%5) + (n4%5) ===0;
+  console.log(isDivisibleBy5);
+
+
+  //Check if n1 greater than n4
+  const isN1GreaterThanN4= n1>n4;
+  console.log(isN1GreaterThanN4);
+
+  //Subtract the first number from the second number.
+  //Multiply the result by the third number.
+  //Find the remainder of dividing the result by the fourth number.
+  const findReminder=((n2-n1)*n3)%n4;
+  console.log(findReminder);
+
+  //Change the way that isOver25 calculates.
+  // So that we do not need to use the NOT operator (!) in other logic comparisons.
+  // Rename the variable as appropriate.
+
+
+
+
+
+
